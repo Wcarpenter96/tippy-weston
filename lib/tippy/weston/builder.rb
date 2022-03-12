f826d7bdd151001221174e7cbe8b2b9f47bc1518
Weston Carpenter <51345538+Wcarpenter96@users.noreply.github.com>
2022-03-12 12:44:28 -0500

    rubocop

--- lib/tippy/weston/builder.rb
+++ lib/tippy/weston/builder.rb
@@ -1,41 +1,42 @@
+# frozen_string_literal: true
+
 module Tippy
-    module Weston
-        class Builder
+  module Weston
+    # Gotta love Builder
+    class Builder
+      def initialize(total:, gratuity:)
+        @total = total
+        @gratuity = gratuity
+      end
 
-            def initialize(total:, gratuity:)
-                @total = total
-                @gratuity = gratuity
-            end
+      def generate
+        return total_accumulator if number_based?
 
-            def generate
-                if number_based?
-                    return total_accumulator
-                end
-                string_based
-            end
+        string_based
+      end
 
-            private
+      private
 
-            def total_accumulator(gratuity = @gratuity)
-                @total += @total * ( gratuity.to_f / 100)
-            end
+      def total_accumulator(gratuity = @gratuity)
+        @total += @total * (gratuity.to_f / 100)
+      end
 
-            def number_based?
-                @gratuity.is_a?(Numeric) || (@gratuity.integer?)
-            end
+      def number_based?
+        @gratuity.is_a?(Numeric) || @gratuity.integer?
+      end
 
-            def string_based
-                case @gratuity.downcase
-                when 'high'
-                    total_accumulator(25.0)
-                when 'standard'
-                    total_accumulator(20.0)
-                when 'low'
-                    total_accumulator(15.0)
-                else
-                    total_accumulator(20.0)
-                end
-            end
+      def string_based
+        case @gratuity.downcase
+        when "high"
+          total_accumulator(25.0)
+        when "standard"
+          total_accumulator(20.0)
+        when "low"
+          total_accumulator(15.0)
+        else
+          total_accumulator(20.0)
         end
+      end
     end
+  end
 end
--- spec/tippy/weston/builder_spec.rb
+++ spec/tippy/weston/builder_spec.rb
@@ -1,10 +1,12 @@
-require 'spec_helper'
+# frozen_string_literal: true
+
+require "spec_helper"
 
 RSpec.describe Tippy::Weston::Builder do
-    describe '#generate' do
-        it 'Accurately generates a tip given a string or integer input' do
-            new_amount = Tippy::Weston::Builder.new(total: 100, gratuity: 20).generate
-            expect(new_amount).to eq(120.0)
-        end
+  describe "#generate" do
+    it "Accurately generates a tip given a string or integer input" do
+      new_amount = Tippy::Weston::Builder.new(total: 100, gratuity: 20).generate
+      expect(new_amount).to eq(120.0)
     end
+  end
 end
